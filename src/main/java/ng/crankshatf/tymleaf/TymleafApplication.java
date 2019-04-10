a92008b5a85eca3727d542658d8a3eadc1599360
Crankshaft <crankshatf@outlook.com>
2019-04-10 22:33:50 +0800

    Initial commit

---
+++ src/main/java/ng/crankshatf/tymleaf/TymleafApplication.java
@@ -0,0 +1,13 @@
+package ng.crankshatf.tymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TymleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ng/crankshatf/tymleaf/TymleafApplicationTests.java
@@ -0,0 +1,16 @@
+package ng.crankshatf.tymleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TymleafApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
